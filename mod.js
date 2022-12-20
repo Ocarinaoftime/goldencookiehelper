610df1554998ded6759c7c6f031d30091cb7227a
Ocarinaoftime <89474990+Ocarinaoftime@users.noreply.github.com>
2022-12-19 20:15:06 -0500

    Create mod.js

---
+++ mod.js
@@ -0,0 +1,20 @@
+window.requestAnimationFrame(loop)
+function loop() {
+    if (document.getElementById("shimmers").hasChildNodes()) {
+        h.innerHTML = `${document.getElementById("shimmers").childElementCount} golden/wrath cookie(s) is on screen!`;
+    } else {
+        h.innerHTML = "No golden/wrath cookies on screen.";
+    }
+    window.requestAnimationFrame(loop);
+}
+var h = document.createElement("div");h.style.position = "absolute";
+h.style.fontSize = '20px';
+h.innerHTML = "No golden/wrath cookies on screen.";
+h.style.left = "550px";
+h.style.top = '100px';
+document.body.append(h);
+document.addEventListener("keypress", (event) => {
+    if (event.key == " ") {
+        Game.ClickCookie();
+    }
+});
